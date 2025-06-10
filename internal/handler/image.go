3c20c1ce230b7174d9a92c413e0c25c78bdc89fd
Hải Tân <221124022310@due.udn.vn>
2025-06-10 23:04:28 +0700

    Hai commit v42

--- internal/handler/image.go
+++ internal/handler/image.go
@@ -1,7 +1,7 @@
 package handler
 
 import (
-	customErr "backend/internal/errors"
+	//customErr "backend/internal/errors"
 	"backend/internal/imagekit"
 	"backend/internal/service"
 	"backend/internal/utils"
@@ -9,18 +9,24 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type ImageHandler struct {
-	imageService service.ImageService
+	imageService    service.ImageService
 	imageKitService imagekit.ImageKitService
 }
 
 func NewImageHandler(imageService service.ImageService, imageKitService imagekit.ImageKitService) *ImageHandler {
 	return &ImageHandler{
-		imageService: imageService,
+		imageService:    imageService,
 		imageKitService: imageKitService,
 	}
+}
+
+type ImageResponseDto struct {
+	ImageURL   string
+	ImageKitID string
 }
 
 func (h *ImageHandler) UploadImages(c *gin.Context) {
@@ -30,6 +36,7 @@ func (h *ImageHandler) UploadImages(c *gin.Context) {
 		utils.JSON(c, http.StatusBadRequest, "Không thể parse muiltipart form", nil)
 		return
 	}
+	defer form.RemoveAll()
 
 	files := form.File["files"]
 	if len(files) == 0 {
@@ -37,21 +44,35 @@ func (h *ImageHandler) UploadImages(c *gin.Context) {
 		return
 	}
 
-	productID := c.PostForm("product_id")
-
-	uploadedImages, err := h.imageService.UploadImages(ctx, files, productID)
-	if err != nil {
-		switch err {
-		case customErr.ErrProductNotFound:
-			utils.JSON(c, http.StatusNotFound, err.Error(), nil)
-		default:
-			fmt.Printf("Lỗi ở UploadImagesService: %v\n", err)
-			utils.JSON(c, http.StatusInternalServerError, "Không thể upload ảnh sản phẩm", nil)
+	var imageResDtos []ImageResponseDto
+	for _, file := range files {
+		fileName := uuid.NewString()
+		imageUrl, imageKitID, err := h.imageKitService.UploadImage(ctx, fileName, file)
+		if err != nil {
+			fmt.Printf("Lỗi up load ảnh %s ở ImageKitService %v\n", fileName, err)
+			utils.JSON(c, http.StatusInternalServerError, "Không thể upload ảnh lên ImageKit", nil)
+			return
 		}
-		return
+
+		imageResDto := ImageResponseDto{
+			ImageURL: imageUrl,
+			ImageKitID: imageKitID,
+		}
+
+		imageResDtos = append(imageResDtos, imageResDto)
 	}
+	// if err != nil {
+	// 	switch err {
+	// 	case customErr.ErrProductNotFound:
+	// 		utils.JSON(c, http.StatusNotFound, err.Error(), nil)
+	// 	default:
+	// 		fmt.Printf("Lỗi ở UploadImagesService: %v\n", err)
+	// 		utils.JSON(c, http.StatusInternalServerError, "Không thể upload ảnh sản phẩm", nil)
+	// 	}
+	// 	return
+	// }
 
 	utils.JSON(c, http.StatusOK, "Upload ảnh sản phẩm thành công", gin.H{
-		"images": uploadedImages,
+		"images": imageResDtos,
 	})
 }
